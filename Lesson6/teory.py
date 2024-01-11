3db717875f333550483e93dc27766eab7e64ffe2
Moroz <moroz.oo.itstep@gmail.com>
2024-01-11 16:19:34 +0200

    Lesson6 Warning

--- Lesson6/teory.py
+++ Lesson6/teory.py
@@ -1,10 +1,14 @@
 try:
-    a = 4
+    a = input()
     print(100/int(a))
 except ZeroDivisionError:
     print(f"Неможливо ділити на 0. Це математичне правило")
 except :
     print(f"неможливо виконати ділення на {a}")
+else:
+    print("блок відпрацював без помилок")
+finally:
+    print("блок завершено ")
 print('-'*20)
 # a = int(input("Enter number"))
 # print(100/a)
@@ -19,5 +23,14 @@ def check_material(amount_of_material, limit_value):
         return "enough material"
     else:
         raise BuildingError(amount_of_material)
-mater = 123
+mater = 400
 check_material(mater,300)
+
+
+import  warnings
+
+warnings.simplefilter("ignore",SyntaxWarning)
+warnings.simplefilter("always",ImportWarning)
+
+warnings.warn("Обережно, тут немає коду", SyntaxWarning)
+warnings.warn("Попередження, модуль не імпортувався", ImportWarning)
